734717b14ddd2617c2acef8e18fdfaaaf32d4d0d
Ian Pride <faithnomoread@yahoo.com>
2024-11-22 13:06:22 -0600

    Working workflow test - 2024-11-22 19:06:21 UTC

--- scripts/buildwindows.ps1
+++ scripts/buildwindows.ps1
@@ -1,18 +1,17 @@
-"Working now????????..."
-# $SCRIPTDIR = $PSScriptRoot
-# $PARENTDIR = (Get-Item $SCRIPTDIR ).parent.FullName
-# $WINDOWSDIR = "${PARENTDIR}\src\windows"
-# $BUILDDIR = "$($PARENTDIR)\build\windows"
+$SCRIPTDIR = $PSScriptRoot
+$PARENTDIR = (Get-Item $SCRIPTDIR ).parent.FullName
+$WINDOWSDIR = "${PARENTDIR}\windows"
+$BUILDDIR = "$($PARENTDIR)\build\windows"
 # $BUILDDIR86 = "$($BUILDDIR)\x86"
-# $BUILDDIR64 = "$($BUILDDIR)\x64"
-# if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
-# {
-#   New-Item "$($BUILDDIR)" -ItemType Directory
-#   if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
-#   {
-#     Exit 255
-#   }
-# }
+$BUILDDIR64 = "$($BUILDDIR)\x64"
+if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
+{
+  New-Item "$($BUILDDIR)" -ItemType Directory
+  if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
+  {
+    Exit 255
+  }
+}
 # if (-Not (Test-Path "$($BUILDDIR86)" -PathType Container))
 # {
 #   New-Item "$($BUILDDIR86)" -ItemType Directory
@@ -21,24 +20,27 @@
 #     Exit 254
 #   }
 # }
-# if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
-# {
-#   New-Item "$($BUILDDIR64)" -ItemType Directory
-#   if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
-#   {
-#     Exit 253
-#   }
-# }
-# Set-Location "$($WINDOWSDIR)"
+if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
+{
+  New-Item "$($BUILDDIR64)" -ItemType Directory
+  if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
+  {
+    Exit 254
+    # Exit 253
+  }
+}
+Set-Location "$($WINDOWSDIR)"
 # MSBuild /p:Configuration=Release /p:Platform=x86
-# if (-Not (Test-Path "$($WINDOWSDIR)\Release\hextodec.exe" -PathType Leaf))
+# if (-Not (Test-Path "$($WINDOWSDIR)\Release\rgbdiff.exe" -PathType Leaf))
 # {
-#   Exit 252
+#   Exit 253
+#   # Exit 252
 # }
-# Move-Item -Path "$($WINDOWSDIR)\Release\hextodec.exe" "$($BUILDDIR86)\hextodec-x86.exe"
-# MSBuild /p:Configuration=Release /p:Platform=x64
-# if (-Not (Test-Path "$($WINDOWSDIR)\x64\Release\hextodec.exe" -PathType Leaf))
-# {
-#   Exit 251
-# }
-# Move-Item -Path "$($WINDOWSDIR)\x64\Release\hextodec.exe" "$($BUILDDIR64)\hextodec-x64.exe"
+# Move-Item -Path "$($WINDOWSDIR)\Release\rgbdiff.exe" "$($BUILDDIR86)\rgbdiff-x86.exe"
+MSBuild /p:Configuration=Release /p:Platform=x64
+if (-Not (Test-Path "$($WINDOWSDIR)\x64\Release\rgbdiff.exe" -PathType Leaf))
+{
+  Exit 253
+  # Exit 251
+}
+Move-Item -Path "$($WINDOWSDIR)\x64\Release\rgbdiff.exe" "$($BUILDDIR64)\rgbdiff-x64.exe"
