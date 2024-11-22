585abd0742fc5d59b807d7edbbea623d2928f2d1
Ian Pride <faithnomoread@yahoo.com>
2024-11-22 11:41:26 -0600

    Working workflow test - 2024-11-22 17:41:26 UTC

---
+++ scripts/buildwindows.ps1
@@ -0,0 +1,43 @@
+# $SCRIPTDIR = $PSScriptRoot
+# $PARENTDIR = (Get-Item $SCRIPTDIR ).parent.FullName
+# $WINDOWSDIR = "${PARENTDIR}\src\windows"
+# $BUILDDIR = "$($PARENTDIR)\build\windows"
+# $BUILDDIR86 = "$($BUILDDIR)\x86"
+# $BUILDDIR64 = "$($BUILDDIR)\x64"
+# if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
+# {
+#   New-Item "$($BUILDDIR)" -ItemType Directory
+#   if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
+#   {
+#     Exit 255
+#   }
+# }
+# if (-Not (Test-Path "$($BUILDDIR86)" -PathType Container))
+# {
+#   New-Item "$($BUILDDIR86)" -ItemType Directory
+#   if (-Not (Test-Path "$($BUILDDIR86)" -PathType Container))
+#   {
+#     Exit 254
+#   }
+# }
+# if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
+# {
+#   New-Item "$($BUILDDIR64)" -ItemType Directory
+#   if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
+#   {
+#     Exit 253
+#   }
+# }
+# Set-Location "$($WINDOWSDIR)"
+# MSBuild /p:Configuration=Release /p:Platform=x86
+# if (-Not (Test-Path "$($WINDOWSDIR)\Release\hextodec.exe" -PathType Leaf))
+# {
+#   Exit 252
+# }
+# Move-Item -Path "$($WINDOWSDIR)\Release\hextodec.exe" "$($BUILDDIR86)\hextodec-x86.exe"
+# MSBuild /p:Configuration=Release /p:Platform=x64
+# if (-Not (Test-Path "$($WINDOWSDIR)\x64\Release\hextodec.exe" -PathType Leaf))
+# {
+#   Exit 251
+# }
+# Move-Item -Path "$($WINDOWSDIR)\x64\Release\hextodec.exe" "$($BUILDDIR64)\hextodec-x64.exe"
