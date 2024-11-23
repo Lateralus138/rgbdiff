dababf622eb5728084239a66652102535c9213f5
Ian Pride <faithnomoread@yahoo.com>
2024-11-22 23:59:12 -0600

    Working workflow test - 2024-11-23 05:59:11 UTC

--- scripts/buildwindows.ps1
+++ scripts/buildwindows.ps1
@@ -8,7 +8,7 @@ if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
   New-Item "$($BUILDDIR)" -ItemType Directory
   if (-Not (Test-Path "$($BUILDDIR)" -PathType Container))
   {
-    Exit 255
+    $global:LASTEXITCODE = 255
   }
 }
 if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
@@ -16,15 +16,15 @@ if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
   New-Item "$($BUILDDIR64)" -ItemType Directory
   if (-Not (Test-Path "$($BUILDDIR64)" -PathType Container))
   {
-    Exit 254
+    $global:LASTEXITCODE = 254
   }
 }
 Set-Location "$WINDOWSDIR"
 MSBuild /p:Configuration=Release /p:Platform=x64
 if (-Not (Test-Path "$($WINDOWSDIR)\x64\Release\rgbdiff.exe" -PathType Leaf))
 {
-  Exit 253
+  $global:LASTEXITCODE = 253
 }
 Move-Item -Path "$($WINDOWSDIR)\x64\Release\rgbdiff.exe" "$($BUILDDIR64)\rgbdiff.exe"
 Set-Location "$PARENTDIR"
-Exit 1
+
